d0da2afe86a614a1cd83cd14dba8c0ab6c999f2a
“isolcat” <308750414@qq.com>
2022-11-19 20:56:35 +0800

    fix test

--- test/dev.test.js
+++ test/dev.test.js
@@ -4,16 +4,15 @@ const inputCode = `
         const a = 10;
         const b = 20;
         console.log(a+b)
-        if(DEBUG){
+        if(TEST){
             console.log("Hello World")
         }
         `;
 
-describe("babel-plugin-dev-debug", () => {
+describe("babel-plugin-devTest", () => {
     it("dev", () => {
         // input
         process.env.NODE_ENV = "development";
-
         const { code } = babel.transform(inputCode, { plugins: [plugin] });
         expect(code).toMatchSnapshot();
     });
@@ -21,7 +20,6 @@ describe("babel-plugin-dev-debug", () => {
     it("prod", () => {
         // input
         process.env.NODE_ENV = "production";
-
         const { code } = babel.transform(inputCode, { plugins: [plugin] });
         expect(code).toMatchSnapshot();
     });
